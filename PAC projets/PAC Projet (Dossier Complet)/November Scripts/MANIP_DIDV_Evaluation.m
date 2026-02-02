74d60a45d5c0214601f41e270db7c2795363590d
Zinedine Abayahia <83831043+azinedine456@users.noreply.github.com>
2026-02-02 15:48:40 +0100

    Deleted obsolete files

--- PAC projets/PAC Projet (Dossier Complet)/November Scripts/MANIP_DIDV_Evaluation.m
+++
@@ -1,8 +0,0 @@
-D = 0.5;  % Duty ratio at max ripple condition
-II=2.7;
-DII = D * E_boost / (L_boost * fdec_boost)      % Inductance [H]
-DVV = II * D / (fdec_boost * C_boost)   % Capacitance [F]
-clear("DVV")
-clear("DII")
-clear("II")
-clear("D")
